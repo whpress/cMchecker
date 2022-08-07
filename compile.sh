3463a56d4d434b33ad4503d71b12ad72d6a9e585
Carson Wilde <wilde.utah@gmail.com>
2022-08-06 19:58:08 -0600

    removed build directory

--- compile.sh
+++ compile.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 # g++ -std=c++20 src/cMchecker.cpp -o build/cMchecker
+
+# make build directory if it does not exist
+if [[ ! -d build ]]
+then
+  mkdir build
+fi
+
+# switch to build directory, cmake and make
 cd build
 cmake ..
 make
